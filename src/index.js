e625f3c3d896dbc6a52e1732259c1ecb2949eebf
ThienHNguyenKMS <thienhnguyen@kms-technology.com>
2023-09-30 23:44:38 +0700

    change path.join

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
-app.set("views", PATH.join(__dirname, "resources/views"));
+app.set("views", PATH.join(__dirname, "resources", "views"));
 
 // routes
 const route = require("./routes");
