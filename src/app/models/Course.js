2775f3bcb3a1a4956d5fa47beab3496d573f4e8b
ThienHNguyenKMS <thienhnguyen@kms-technology.com>
2023-10-01 17:26:31 +0700

    create course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,21 +1,31 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+const slug = require("mongoose-slug-updater");
+
+mongoose.plugin(slug);
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const Course = new Schema({
-  name: { type: String, default: "default name" },
-  description: { type: String, default: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." },
-  image: {
-    type: String,
-    default:
-      "https://images.unsplash.com/photo-1611267254323-4db7b39c732c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y3V0ZSUyMGNhdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+const Course = new Schema(
+  {
+    name: { type: String, default: "Default course" },
+    description: {
+      type: String,
+      default:
+        "Lorem Ipsum is simply dummy text of the printing of Lorem Ipsum.",
+    },
+    image: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1611267254323-4db7b39c732c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y3V0ZSUyMGNhdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+    },
+    videoId: { type: String, default: "Rso7JCzJ8ms" },
+    slug: { type: String, slug: "name", unique: true },
   },
-  slug: { type: String, default: "test" },
-  videoId: {type: String, default: "Rso7JCzJ8ms"},
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Course", Course);
 // => "Course" will be converted auto -> courses (plural, lower case) -> to be the same in Mongo
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
-console.log("courses.js route");
 const courseController = require("../app/controllers/CourseController");
 
+router.get("/create", courseController.create);
+router.post("/store", courseController.store);
 router.get("/:slug", courseController.show);
 
 module.exports = router;
